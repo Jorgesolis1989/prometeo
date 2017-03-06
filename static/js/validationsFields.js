c250a70e1bf21b2c841ddb57d9314d57adfe4d76
Martha L <marthikaliliana@gmail.com>
2017-03-05 21:36:15 -0500

    mostrar contrasenas

---
+++ static/js/validationsFields.js
@@ -0,0 +1,9 @@
+/**
+ * Created by martha on 5/03/17.
+ */
+
+function showPassword() {
+    document.getElementById("passwordA").type = 'text';
+    document.getElementById("passwordN").type = 'text';
+    document.getElementById("passwordC").type = 'text';
+}
