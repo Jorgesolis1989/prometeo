2b95a50717c4c0563bfab06089d209d709cd1d17
Martha L <marthikaliliana@gmail.com>
2017-03-20 16:26:37 -0500

    menu carpetas

--- static/js/validationsFields.js
+++ static/js/validationsFields.js
@@ -3,7 +3,21 @@
  */
 
 function showPassword() {
-    document.getElementById("passwordA").type = 'text';
-    document.getElementById("passwordN").type = 'text';
-    document.getElementById("passwordC").type = 'text';
+    var x = document.getElementById("demo-form-checkbox").checked;
+    alert(x)
+    if (document.getElementById("demo-form-checkbox").checked = true)
+    {
+        document.getElementById("passwordA").type = 'text';
+        document.getElementById("passwordN").type = 'text';
+        document.getElementById("passwordC").type = 'text';
+    }
+    else
+        if (document.getElementById("demo-form-checkbox").checked = false)
+        {
+            document.getElementById("demo-form-checkbox").checked = false;
+            document.getElementById("passwordA").type = 'password';
+            document.getElementById("passwordN").type = 'password';
+            document.getElementById("passwordC").type = 'password';
+        }
+
 }
