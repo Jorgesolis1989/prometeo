0baec06cd92c7210da86dd47340d0585b7e14057
jorgesolis1989 <jorgesolis1989@gmail.com>
2017-04-20 23:42:45 -0500

    trabajando a mensajes

--- bandeja_entrada/views.py
+++ bandeja_entrada/views.py
@@ -6,7 +6,7 @@ def listar_bandeja(request):
     usuario = get_object_or_404(User, email=request.user.email)
     usuario_web = Usuario_Web.objects.get(email_usrio= usuario.email)
 
-    documentos_correo = Documentos_Correo.objects.filter(email_usrio=usuario.email)
+    documentos_correo = Documentos_Correo.objects.filter(email_usrio=usuario.email).order_by('-fcha_dcmnto')
 
     return render(request, 'bandeja_entrada.html',{'empresas_vinculadas': cargar_empresas_vinculadas(request),
                                                    'documentos_correo': documentos_correo,
